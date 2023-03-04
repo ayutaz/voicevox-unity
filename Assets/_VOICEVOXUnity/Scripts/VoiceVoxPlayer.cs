db7684c679cac5cd0e12d185d308fe83838d9a65
yousan <ka1357amnbpdr@gmail.com>
2023-03-05 02:28:29 +0900

    feat: save audio clip

--- Assets/_VOICEVOXUnity/Scripts/VoiceVoxPlayer.cs
+++ Assets/_VOICEVOXUnity/Scripts/VoiceVoxPlayer.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
 
 namespace _VoiceVoxUnity.Scripts
 {
     public class VoiceVoxPlayer : MonoBehaviour
     {
+        private const string AudioPath = "Assets/_VoiceVoxUnity/Audio";
         private AudioSource _audioSource;
         private VoiceVoxConnection _voiceVoxConnection;
 
@@ -17,6 +20,9 @@ namespace _VoiceVoxUnity.Scripts
         {
             var text = "一緒に冒険をしに行こう";
             var clip = await _voiceVoxConnection.TranslateTextToAudioClip(text);
+            var path = Path.Combine(AudioPath, "voice.asset");
+            AssetDatabase.CreateAsset(clip, path);
+            AssetDatabase.Refresh();
             _audioSource.clip = clip;
             _audioSource.Play();
         }
